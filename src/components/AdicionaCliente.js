ec323e4d1a2a88b9a3290f3a96c906b02f6a25f2
karolina mendes <karoolina.sm@gmail.com>
2021-02-15 18:12:35 -0300

    parte final

--- src/components/AdicionaCliente.js
+++ src/components/AdicionaCliente.js
@@ -1,60 +1,82 @@
-import {Formik} from 'formik'
+import {Formik, Field , useField} from 'formik'
 import React from 'react'
+import * as yup from 'yup'
 
+const Campo = props =>{
+    const [field, meta] = useField(props)
+    return(
+        <div className="form-group">
+            <label htmlFor={props.id}>{props.label}</label>
+        <input
+        {...field}
+        {...props}
+        className={meta.error && meta.touched ? 'error-campo': ''}/>
+        </div>
+    )
+}
 function AdicionaCliente() {
+
+    const esquema = yup.object({
+        nome: yup.string()
+        .required("O campo é obrigatorio"),
+        email: yup.string()
+        .required("E-mail é obrigatorio")
+        .email("Formato inválido"),
+        nascimento: yup.date()
+        .required('Data de nascimento é obrigatoria')
+        .max(new Date(), 'data inválida')
+
+    })
     
     return(
         <>
         <h1>Cadastro de Clientes</h1>
         <Formik 
         initialValues={{nome: '', email:'', nascimento:''}}
-        validate={(values)=>{
-            const errors = {};
-            if(!values.nome){
-                errors.nome = "O nome é obrigatório"
-            }
-            if(!values.email){
-                errors.email = "O E-mail é obrigatório"
-            }else if(  !/^[A-Z0-9_%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email) ){
-             errors.email = "O e-mail é invalido"
-            }
-            if(!values.nascimento){
-                errors.nascimento = "A data é obrigatória"
-            }
-            return errors;
-        }}
+        // validate={(values)=>{
+        //     const errors = {};
+        //     if(!values.nome){
+        //         errors.nome = "O nome é obrigatório"
+        //     }
+        //     if(!values.email){
+        //         errors.email = "O E-mail é obrigatório"
+        //     }else if(  !/^[A-Z0-9_%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email) ){
+        //      errors.email = "O e-mail é invalido"
+        //     }
+        //     if(!values.nascimento){
+        //         errors.nascimento = "A data é obrigatória"
+        //     }
+        //     return errors;
+        // }}
+        validationSchema={esquema}
         onSubmit={(values)=>{
             alert(JSON.stringify(values))
         }}
         >
           {(props)=>(
+            //   Field substitui o input
             <form onSubmit={props.handleSubmit} noValidate>
-                <div className="form-group">
-                    <label htmlFor="nome">Nome</label>
-                    <input  id="nome" name="nome" type="text"
-                    value={props.values.nome}
-                    onChange={props.handleChange} 
-                    onBlur={props.handleBlur}/>
+              
+                    
+                    <Campo  id="nome" name="nome" 
+                    type="text"
+                    label="Nome "/>
+                   
                     {props.errors.nome && props.touched.nome ? (<div className="error-campo">{props.errors.nome}</div>): ''}
-                </div>
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input  id="email" name="email"
+                
+                
+                    <Campo  id="email" name="email"
                      type="email" 
-                     value={props.values.email}
-                     onChange={props.handleChange}
-                     onBlur={props.handleBlur}/>
+                     label="Email  "/>
+                     
                       {props.errors.email  && props.touched.email ? (<div className="error-campo">{props.errors.email}</div>): ''}
-                </div>
-                <div className="form-group">
-                    <label htmlFor="date">Data de Nascimento</label>
-                    <input  id="nascimento" name="nascimento"
+                
+                 <Campo  id="nascimento" name="nascimento"
                      type="date" 
-                     value ={ props.values.nascimento }
-                     onChange={props.handleChange}
-                     onBlur={props.handleBlur}/>
+                     label="Data de nascimento"
+                    />
                       {props.errors.nascimento && props.touched.nascimento ? (<div className="error-campo">{props.errors.nascimento}</div>): ''}
-                </div>
+                
             <button type="submit">Adicionar</button>
             </form>
             )}
